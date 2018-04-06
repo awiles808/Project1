a4ffdd1725e2c1ff79d695d372f872644639f013
awiles808 <awiles808@gmail.com>
2018-04-06 11:04:00 -0600

    Took out Canvas...for now

--- project1.js
+++ project1.js
@@ -1,4 +1,26 @@
-$(function(){
+$(function(){                   //Using jQuery to get the DOM Ready.
 
 
-})
+var  = $(document.getElementsByTagName(''));        //jS&jQuery linked. Declaring  Variable
+    console.log('');                                      //Testing That The Variable is Defined .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
